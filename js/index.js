d9ca65ff32124407408ba1785b86614b23d0fb6d
Maureen Njihia <maureennjihia468@gmail.com>
2021-07-26 08:12:27 +0300

    Add toggle functions for what we do icons

---
+++ js/index.js
@@ -0,0 +1,12 @@
+$(".design-icon").click(function(){
+    $(".icon-text").toggle();
+  });
+
+$(".dev-icon").click(function(){
+    $(".icon-text2").toggle();
+  });  
+
+$(".product-icon").click(function(){
+    $(".icon-text3").toggle();
+  });  
+  
