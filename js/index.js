ce5e435a34a4804bcb610c1e68fa80f252d58027
Maureen Njihia <maureennjihia468@gmail.com>
2021-07-26 08:49:09 +0300

    Add portfolio hover effect functions

--- js/index.js
+++ js/index.js
@@ -10,3 +10,98 @@ $(".product-icon").click(function(){
     $(".icon-text3").toggle();
   });  
   
+  $(".div1").hover(function () {
+    // over
+    $(".po1").animate({ opacity: 1 }, 50);
+    $(".pn1").addClass("portfolio-hover-effect");
+}
+    , function () {
+        // out
+        $(".po1").animate({ opacity: 0 }, 50);
+        $(".pn1").removeClass("portfolio-hover-effect");
+    }
+);
+
+$(".div2").hover(function () {
+    // over
+    $(".po2").animate({ opacity: 1 }, 50);
+    $(".pn2").addClass("portfolio-hover-effect");
+}
+    , function () {
+        // out
+        $(".po2").animate({ opacity: 0 }, 50);
+        $(".pn2").removeClass("portfolio-hover-effect");
+    }
+);
+
+$(".div3").hover(function () {
+    // over
+    $(".po3").animate({ opacity: 1 }, 50);
+    $(".pn3").addClass("portfolio-hover-effect");
+}
+    , function () {
+        // out
+        $(".po3").animate({ opacity: 0 }, 50);
+        $(".pn3").removeClass("portfolio-hover-effect");
+    }
+);
+
+$(".div4").hover(function () {
+    // over
+    $(".po4").animate({ opacity: 1 }, 50);
+    $(".pn4").addClass("portfolio-hover-effect");
+}
+    , function () {
+        // out
+        $(".po4").animate({ opacity: 0 }, 50);
+        $(".pn4").removeClass("portfolio-hover-effect");
+    }
+);
+
+$(".div5").hover(function () {
+    // over
+    $(".po5").animate({ opacity: 1 }, 50);
+    $(".pn5").addClass("portfolio-hover-effect");
+}
+    , function () {
+        // out
+        $(".po5").animate({ opacity: 0 }, 50);
+        $(".pn5").removeClass("portfolio-hover-effect");
+    }
+);
+
+$(".div6").hover(function () {
+    // over
+    $(".po6").animate({ opacity: 1 }, 50);
+    $(".pn6").addClass("portfolio-hover-effect");
+}
+    , function () {
+        // out
+        $(".po6").animate({ opacity: 0 }, 50);
+        $(".pn6").removeClass("portfolio-hover-effect");
+    }
+);
+
+$(".div7").hover(function () {
+    // over
+    $(".po7").animate({ opacity: 1 }, 50);
+    $(".pn7").addClass("portfolio-hover-effect");
+}
+    , function () {
+        // out
+        $(".po7").animate({ opacity: 0 }, 50);
+        $(".pn7").removeClass("portfolio-hover-effect");
+    }
+);
+
+$(".div8").hover(function () {
+    // over
+    $(".po8").animate({ opacity: 1 }, 50);
+    $(".pn8").addClass("portfolio-hover-effect");
+}
+    , function () {
+        // out
+        $(".po8").animate({ opacity: 0 }, 50);
+        $(".pn8").removeClass("portfolio-hover-effect");
+    }
+);
